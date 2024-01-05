3069f368e207424a9c2e8039425006fa6393e875
Jens <69081683+jenslys@users.noreply.github.com>
2024-01-05 15:19:46 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nrkdl",
-    version="1.2.0",
+    version="1.2.1",
     author="jenlys",
     description="Download movies/tv-shows from nrk.no",
     long_description=long_description,
